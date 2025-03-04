aae85df52c1f1e2f1bce1b106da08eed062b11e8
Tanishka Gupta <tanishkagupta152621@gmail.com>
2025-03-04 06:49:35 +0000

    Sync LeetCode submission Runtime - 3 ms (94.32%), Memory - 12.5 MB (17.37%)

---
+++ 0009-palindrome-number/solution.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        return str(x) == str(x)[::-1]
+        solution=solution()
+        x=input()
+        if solution.isPalindrome(x):
+            print("True")
+        else:
+            print("False")
+        
+
+        
