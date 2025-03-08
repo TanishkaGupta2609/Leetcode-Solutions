07f17315e77ab6b2077ef1f9b61fb5baef8e4b7c
Tanishka Gupta <tanishkagupta152621@gmail.com>
2025-03-08 05:58:00 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.5 MB (28.96%)

---
+++ 0349-intersection-of-two-arrays/solution.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def set_intersection(self, set1, set2):
+        return[x for x in set1 if x in set2]
+    def intersection(self,nums1,nums2):
+        set1=set(nums1)
+        set2=set(nums2)
+        return self.set_intersection(set1,set2)
