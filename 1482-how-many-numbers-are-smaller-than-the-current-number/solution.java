7ae9c3341f20210362e4cf074191e0b2ced41a2c
Tanishka Gupta <tanishkagupta152621@gmail.com>
2025-09-24 12:49:02 +0000

    Sync LeetCode submission Runtime - 7 ms (82.21%), Memory - 44.9 MB (99.39%)

--- 1482-how-many-numbers-are-smaller-than-the-current-number/solution.java
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/solution.java
@@ -1,15 +1,15 @@
 class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
-        int[] freq = new int[101];              // values 0..100
-        for (int x : nums) freq[x]++;
-
-        // prefix sums: freq[i] = count of numbers <= i
-        for (int i = 1; i < 101; i++) freq[i] += freq[i - 1];
-
-        int[] ans = new int[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            int x = nums[i];
-            ans[i] = (x == 0) ? 0 : freq[x - 1];
+        int n=nums.length;
+        int ans[]=new int[n];
+        for(int i=0;i<n;i++){
+            int count=0;
+            for(int j=0;j<n;j++){
+                if(nums[j]<nums[i]){
+                    count++;
+                }
+            }
+            ans[i]=count;
         }
         return ans;
     }
