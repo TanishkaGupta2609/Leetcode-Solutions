9de09010bc1641f920e6aaf68d9217912c23c8fb
Tanishka Gupta <tanishkagupta152621@gmail.com>
2025-11-24 07:15:21 +0000

    Sync LeetCode submission Runtime - 15 ms (61.53%), Memory - 48.7 MB (35.04%)

---
+++ 0503-next-greater-element-ii/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int[] nge=new int[nums.length];
+        int n=nums.length;
+        Stack<Integer> stack=new Stack<>();
+        for(int i=2*n-1;i>=0;i--){
+            while(!stack.isEmpty() && stack.peek()<=nums[i%n]){
+                stack.pop();
+            }
+            if(i<n){
+                if(stack.isEmpty())nge[i]=-1;
+                else nge[i]=stack.peek();
+            }
+            stack.push(nums[i%n]);
+        }
+        return nge;
+    }
+}
