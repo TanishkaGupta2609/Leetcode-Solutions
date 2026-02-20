941f0fade33b50b17f2221decdd30d18ffd63838
Tanishka Gupta <tanishkagupta152621@gmail.com>
2026-02-20 06:37:21 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 44.4 MB (20.79%)

---
+++ 0078-subsets/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans=new ArrayList<>();
+        helper(nums,0,new ArrayList<>(),ans);
+        return ans;
+    }
+    public void helper(int[] nums,int idx,List<Integer>current,List<List<Integer>>ans){
+        if(idx==nums.length){
+            ans.add(new ArrayList<>(current));
+            return;
+        }
+        current.add(nums[idx]);
+        helper(nums,idx+1,current,ans);
+        current.remove(current.size()-1);
+        helper(nums,idx+1,current,ans);
+    }
+}
