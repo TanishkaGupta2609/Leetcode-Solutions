2a4f7cce506890d213373f3208321031827ec993
Tanishka Gupta <tanishkagupta152621@gmail.com>
2026-02-24 11:24:13 +0000

    Sync LeetCode submission Runtime - 1 ms (98.96%), Memory - 42.9 MB (15.02%)

---
+++ 0263-ugly-number/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isUgly(int n) {
+      if(n<=0)return false;
+      int[] fac={2,3,5};
+      for(int f:fac){
+        while(n%f==0){
+            n/=f;
+        }
+      }
+      return n==1;
+    }
+}
