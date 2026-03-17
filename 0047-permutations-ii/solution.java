20ad2372ff3a78608270ab114ef9e14987ba5744
Tanishka Gupta <tanishkagupta152621@gmail.com>
2026-03-17 04:49:41 +0000

    Sync LeetCode submission Runtime - 2 ms (98.64%), Memory - 47.2 MB (9.65%)

--- 0047-permutations-ii/solution.java
+++ 0047-permutations-ii/solution.java
@@ -4,26 +4,25 @@ class Solution {
         helper(nums,0,ans);
         return ans;
     }
+    public void swap(int[] nums,int i,int j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
     public void helper(int[] nums,int idx,List<List<Integer>> ans){
         if(idx==nums.length){
-            List<Integer> temp=new ArrayList<>();
-            for(int num: nums){
-                temp.add(num);
-            }
-            ans.add(temp);
-            return;
+           List<Integer> temp=new ArrayList<>();
+           for(int x:nums)temp.add(x);
+           ans.add(temp);
+           return;
         }
-        HashSet<Integer> set=new HashSet<>();
+        HashSet set=new HashSet<>();
         for(int i=idx;i<nums.length;i++){
             if(set.contains(nums[i]))continue;
             set.add(nums[i]);
-            int temp=nums[idx];
-            nums[idx]=nums[i];
-            nums[i]=temp;
+            swap(nums,idx,i);
             helper(nums,idx+1,ans);
-           int t=nums[idx];
-            nums[idx]=nums[i];
-            nums[i]=t; 
+            swap(nums,idx,i);
         }
     }
 }
