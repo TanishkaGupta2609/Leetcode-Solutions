bc7670afad912986d467e649280a1df5f26ff542
Tanishka Gupta <tanishkagupta152621@gmail.com>
2026-03-17 04:34:24 +0000

    Sync LeetCode submission Runtime - 1 ms (87.24%), Memory - 45.2 MB (95.69%)

--- 0046-permutations/solution.java
+++ 0046-permutations/solution.java
@@ -1,26 +1,26 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
+        List<Integer> cur=new ArrayList<>();
         List<List<Integer>> ans=new ArrayList<>();
-        helper(nums,0,ans);
+        helper(nums,0,cur,ans);
         return ans;
     }
-    public void helper(int[] nums,int idx,List<List<Integer>> ans){
+    public void swap(int[] nums,int i,int j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
+    public void helper(int[] nums,int idx,List<Integer> cur,List<List<Integer>> ans){
         if(idx==nums.length){
-            List<Integer> temp=new ArrayList<>();
-            for(int num:nums){
-                temp.add(num);
-            }
-            ans.add(temp);
+            ans.add(new ArrayList<>(cur));
             return;
         }
         for(int i=idx;i<nums.length;i++){
-            int temp=nums[idx];
-            nums[idx]=nums[i];
-            nums[i]=temp;
-            helper(nums,idx+1,ans);
-             int t=nums[idx];
-            nums[idx]=nums[i];
-            nums[i]=t;
+            cur.add(nums[i]);
+            swap(nums,idx,i);
+            helper(nums,idx+1,cur,ans);
+            cur.remove(cur.size()-1);
+            swap(nums,idx,i);
         }
     }
 }
