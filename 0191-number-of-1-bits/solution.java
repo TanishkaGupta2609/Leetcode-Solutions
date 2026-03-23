b9de062d8735d15e9ff63261ec578ff75862b792
Tanishka Gupta <tanishkagupta152621@gmail.com>
2026-03-23 05:32:30 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.4 MB (48.10%)

--- 0191-number-of-1-bits/solution.java
+++ 0191-number-of-1-bits/solution.java
@@ -2,8 +2,10 @@ class Solution {
     public int hammingWeight(int n) {
         int cnt=0;
         while(n!=0){
-            n=n&(n-1);
-            cnt++;
+            if((n&1)==1){
+                cnt++;
+            }
+            n=(n>>1);
         }
         return cnt;
     }
