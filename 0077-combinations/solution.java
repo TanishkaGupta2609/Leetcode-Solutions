5f7b855be96c2927fbec1659577e72ec60b80d83
Tanishka Gupta <tanishkagupta152621@gmail.com>
2026-04-17 13:24:48 +0000

    Sync LeetCode submission Runtime - 16 ms (88.35%), Memory - 97.7 MB (92.79%)

--- 0077-combinations/solution.java
+++ 0077-combinations/solution.java
@@ -1,17 +1,18 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> ans=new ArrayList<>();
-        helper(n,k,1,new ArrayList<>(),ans);
+        List<Integer> cur=new ArrayList<>();
+        helper(ans,cur,n,k,1);
         return ans;
     }
-    public void helper(int n,int k,int idx,List<Integer> cur,List<List<Integer>> ans){
+    public void helper(List<List<Integer>> ans,List<Integer> cur,int n,int k,int idx){
         if(cur.size()==k){
             ans.add(new ArrayList<>(cur));
             return;
         }
         for(int i=idx;i<=n-(k-cur.size())+1;i++){
             cur.add(i);
-            helper(n,k,i+1,cur,ans);
+            helper(ans,cur,n,k,i+1);
             cur.remove(cur.size()-1);
         }
     }
