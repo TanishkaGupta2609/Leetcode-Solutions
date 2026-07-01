9b64c641f6bfaf8b8c6d72208ffa05cf0e1436a8
Tanishka Gupta <tanishkagupta152621@gmail.com>
2026-07-01 11:21:23 +0000

    Sync LeetCode submission Runtime - 32 ms (36.60%), Memory - 95.7 MB (27.88%)

---
+++ 0128-longest-consecutive-sequence/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        HashSet<Integer> set=new HashSet<>();
+        int ans=0;
+        for(int i: nums){
+            set.add(i);
+        }
+        for(int i: set){
+            if(!set.contains(i-1)){
+            int len=1;
+            while(set.contains(i+len)){
+                len++;
+            }
+            
+            ans=Math.max(ans,len);
+            }
+        }
+        return ans;
+            }
+}  
