0e06d88ab9be5f9a1f549d4e190ec7e302168350
Tanishka Gupta <tanishkagupta152621@gmail.com>
2026-07-15 03:59:34 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 48.4 MB (31.56%)

--- 0792-binary-search/solution.java
+++ 0792-binary-search/solution.java
@@ -1,21 +1,17 @@
 class Solution {
     public int search(int[] nums, int target) {
-       int s=0;
-       int e=nums.length-1;
-       if(s>e)return -1;
-       return binary(nums,s,e,target);
-    }
-    public int binary(int[] nums,int s,int e,int target){
-        if(s>e)return -1;
-        int m=e-(e-s)/2;
-        if(nums[m]==target){
-            return m;
+        int s=0;
+        int e=nums.length-1;
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            if(target<nums[mid]){
+                e=mid-1;
+            }else if(target>nums[mid]){
+                s=mid+1;
+            }else{
+                return mid;
+            }
         }
-        else if(nums[m]>target){
-            return binary(nums,s,m-1,target);
-        }
-        else{
-            return binary(nums,m+1,e,target);
-        }
+        return -1;
     }
 }
