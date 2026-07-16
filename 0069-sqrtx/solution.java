318fd159e032f335852873109500b06206a5da16
Tanishka Gupta <tanishkagupta152621@gmail.com>
2026-07-16 05:27:14 +0000

    Sync LeetCode submission Runtime - 1 ms (97.59%), Memory - 42.8 MB (23.48%)

--- 0069-sqrtx/solution.java
+++ 0069-sqrtx/solution.java
@@ -1,5 +1,19 @@
 class Solution {
     public int mySqrt(int x) {
-        return (int)Math.sqrt(x);
+        if(x==0)return 0;
+        int s=1;
+        int e=x/2;
+        int ans=1;
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            if(mid<=x/mid){
+                ans=mid;
+                s=mid+1;
+            }
+           else{
+            e=mid-1;
+           }
+        }
+        return ans;
     }
 }
