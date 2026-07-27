b5894a66e0a83c4f551fea18d619699c7683614f
Tanishka Gupta <tanishkagupta152621@gmail.com>
2026-07-27 17:08:06 +0530

    Time: 0 ms (100%), Space: 44.2 MB (8.74%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n=matrix.length;
+        //int m=matrix[0].length;
+        for(int i=0;i<n;i++){
+                if(binarySearch(matrix[i],target))return true;
+        }
+        return false;
+    }
+    public boolean binarySearch(int[] arr,int t){
+        int low=0;
+        int high=arr.length-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(arr[mid]==t){
+                return true;
+            }else if(arr[mid]>t){
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+        return false;
+    }
+}
