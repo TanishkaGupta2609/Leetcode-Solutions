f354b18b754bf7dab029944ed7bfca1bd7824966
Tanishka Gupta <tanishkagupta152621@gmail.com>
2026-08-20 14:36:45 +0530

    Time: 1 ms (99.76%), Space: 47.4 MB (39.03%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,16 +1,15 @@
 class Solution {
     public int jump(int[] nums) {
-        int jumps = 0, end = 0, farthest = 0;
-
-        for (int i = 0; i < nums.length - 1; i++) {
-            farthest = Math.max(farthest, i + nums[i]);
-
-            if (i == end) {
-                jumps++;
-                end = farthest;
+        int maxIdx=0;//farthest he can go
+        int jump=0;
+        int currentEnd=0;// currently standing at which idx
+        for(int i=0;i<nums.length-1;i++){
+            maxIdx=Math.max(maxIdx,i+nums[i]);
+            if(i==currentEnd){
+                jump++;
+                currentEnd=maxIdx;
             }
         }
-
-        return jumps;
+        return jump;
     }
 }
