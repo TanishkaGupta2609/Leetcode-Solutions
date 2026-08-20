916b99615798d6cccef184e65c97120b24e4a728
Tanishka Gupta <tanishkagupta152621@gmail.com>
2026-08-20 14:17:44 +0530

    Time: 2 ms (88.93%), Space: 47.7 MB (81.75%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,13 +1,10 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int maxReach = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-            if (i > maxReach) return false;
-
-            maxReach = Math.max(maxReach, i + nums[i]);
+        int idx=0;
+        for(int i=0;i<nums.length;i++){
+            if(i>idx)return false;
+            idx=Math.max(idx,nums[i]+i);
         }
-
         return true;
     }
 }
