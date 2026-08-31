42ad934f33e5833d78366f6f0e074035c6659916
Tanishka Gupta <tanishkagupta152621@gmail.com>
2026-08-31 17:35:09 +0530

    Time: 1 ms (100%), Space: 46.4 MB (42.15%) - LeetHub

---
+++ 1094-car-pooling/1094-car-pooling.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean carPooling(int[][] trips, int capacity) {
+        int[] passengers=new int[1001];
+        int n=trips.length;
+        for(int[] trip:trips){
+            int cnt=trip[0];
+            int from=trip[1];
+            int to=trip[2];
+            passengers[from]+=cnt;
+            passengers[to]-=cnt;
+        }
+        int cur=0;
+        for(int people:passengers){
+            cur+=people;
+            if(cur>capacity){
+                return false;
+            }
+        }
+        return true;
+    }
+}
