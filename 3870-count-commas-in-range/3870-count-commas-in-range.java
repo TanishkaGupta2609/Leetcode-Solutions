ac6fdee83c7ddecb94c09e7a96cc83e9e2084f16
Tanishka Gupta <tanishkagupta152621@gmail.com>
2026-09-08 10:29:44 +0530

    Time: 1 ms (83.65%), Space: 42.3 MB (85.26%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,20 +1,24 @@
 class Solution {
-    public int countCommas(int n) {
-        if(n==100000)return 99001;
-        int sum=0;
-        for(int i=1;i<=n;i++){
-            sum+=helper(i);
-        }
-        return sum;
-    }
-    public int helper(int temp){
-        int cnt=0;
-        while(temp>0){
-            int digit=temp%10;
-            cnt++;
-            temp/=10;
-        }
-        if(cnt<4)return 0;
-       return cnt/3;
+    // public int countCommas(int n) {
+    //     if(n==100000)return 99001;
+    //     int sum=0;
+    //     for(int i=1;i<=n;i++){
+    //         sum+=helper(i);
+    //     }
+    //     return sum;
+    // }
+    // public int helper(int temp){
+    //     int cnt=0;
+    //     while(temp>0){
+    //         int digit=temp%10;
+    //         cnt++;
+    //         temp/=10;
+    //     }
+    //     if(cnt<4)return 0;
+    //    return cnt/3;
+    // }
+    public int countCommas(int n){
+        if(n<1000)return 0;
+        return (n-1000)+1;
     }
 }
