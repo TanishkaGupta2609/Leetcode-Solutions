0d056e2abdb05860d3cf7eeaa4ccb87f9af9d49a
Tanishka Gupta <tanishkagupta152621@gmail.com>
2026-09-09 11:10:16 +0530

    Time: 2 ms (99.85%), Space: 52 MB (35.47%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,27 +1,77 @@
+// class Solution {
+//     public int candy(int[] ratings) {
+//         int n=ratings.length;
+//         int sum=n;
+//         int i=1;
+//         while(i<n){
+//             if(ratings[i]==ratings[i-1]){
+//                 i++;
+//                 continue;
+//             }
+//             int peak=1;
+//             while(i<n && ratings[i]>ratings[i-1]){
+//                 peak++;
+//                 sum+=peak;
+//                 i++;
+//             }
+//             int down=1;
+//             while(i<n && ratings[i]<ratings[i-1]){
+//                 down++;
+//                 sum+=down;
+//                 i++;
+//             }
+//            sum-=Math.min(peak,down);
+//         }
+//         return sum;
+//     }
+// }
+
 class Solution {
+    // Function to calculate the minimum number of candies 
     public int candy(int[] ratings) {
+
+        // Get number of children
         int n = ratings.length;
-        int[] candy = new int[n];
 
-        Arrays.fill(candy, 1);
+        // Initially give 1 candy to each child
+        int candies = n;
 
-        for (int i = 1; i < n; i++) {
-            if (ratings[i] > ratings[i - 1]) {
-                candy[i] = candy[i - 1] + 1;
+        // Start from second child
+        int i = 1;
+
+        while (i < n) {
+
+            // Skip equal ratings
+            if (ratings[i] == ratings[i - 1]) {
+                i++;
+                continue;
             }
-        }
 
-        for (int i = n - 2; i >= 0; i--) {
-            if (ratings[i] > ratings[i + 1]) {
-                candy[i] = Math.max(candy[i], candy[i + 1] + 1);
+            // Initialize increasing slope counter
+            int peak = 0;
+
+            // Traverse strictly increasing ratings
+            while (i < n && ratings[i] > ratings[i - 1]) {
+                peak++;
+                candies += peak;
+                i++;
             }
+
+            // Initialize decreasing slope counter
+            int valley = 0;
+
+            // Traverse strictly decreasing ratings
+            while (i < n && ratings[i] < ratings[i - 1]) {
+                valley++;
+                candies += valley;
+                i++;
+            }
+
+            // Remove overlapping candy at the peak
+            candies -= Math.min(peak, valley);
         }
 
-        int ans = 0;
-        for (int x : candy) {
-            ans += x;
-        }
-
-        return ans;
+        // Return total candies required
+        return candies;
     }
 }
