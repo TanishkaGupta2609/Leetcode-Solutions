60689eee72cb96b0bec12f044d113f1a924d5ca5
Tanishka Gupta <tanishkagupta152621@gmail.com>
2026-09-09 11:13:14 +0530

    Time: 2 ms (99.85%), Space: 52.4 MB (13.81%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,77 +1,29 @@
-// class Solution {
-//     public int candy(int[] ratings) {
-//         int n=ratings.length;
-//         int sum=n;
-//         int i=1;
-//         while(i<n){
-//             if(ratings[i]==ratings[i-1]){
-//                 i++;
-//                 continue;
-//             }
-//             int peak=1;
-//             while(i<n && ratings[i]>ratings[i-1]){
-//                 peak++;
-//                 sum+=peak;
-//                 i++;
-//             }
-//             int down=1;
-//             while(i<n && ratings[i]<ratings[i-1]){
-//                 down++;
-//                 sum+=down;
-//                 i++;
-//             }
-//            sum-=Math.min(peak,down);
-//         }
-//         return sum;
-//     }
-// }
-
 class Solution {
-    // Function to calculate the minimum number of candies 
     public int candy(int[] ratings) {
-
-        // Get number of children
-        int n = ratings.length;
-
-        // Initially give 1 candy to each child
-        int candies = n;
-
-        // Start from second child
-        int i = 1;
-
-        while (i < n) {
-
-            // Skip equal ratings
-            if (ratings[i] == ratings[i - 1]) {
+        int n=ratings.length;
+        int sum=n;
+        int i=1;
+        while(i<n){
+            if(ratings[i]==ratings[i-1]){
                 i++;
                 continue;
             }
-
-            // Initialize increasing slope counter
-            int peak = 0;
-
-            // Traverse strictly increasing ratings
-            while (i < n && ratings[i] > ratings[i - 1]) {
+            int peak=0;
+            while(i<n && ratings[i]>ratings[i-1]){
                 peak++;
-                candies += peak;
+                sum+=peak;
                 i++;
             }
-
-            // Initialize decreasing slope counter
-            int valley = 0;
-
-            // Traverse strictly decreasing ratings
-            while (i < n && ratings[i] < ratings[i - 1]) {
-                valley++;
-                candies += valley;
+            int down=0;
+            while(i<n && ratings[i]<ratings[i-1]){
+                down++;
+                sum+=down;
                 i++;
             }
-
-            // Remove overlapping candy at the peak
-            candies -= Math.min(peak, valley);
+            sum-=Math.min(peak,down);
         }
-
-        // Return total candies required
-        return candies;
+        
+        return sum;
     }
 }
+
